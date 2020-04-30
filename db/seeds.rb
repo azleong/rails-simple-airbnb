9c53b37404d36486ed89ca30781adf5923db2ff5
Andy Leong <mindreko@gmail.com>
2020-04-30 19:48:03 +1000

    airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,17 @@
+puts "cleaning"
+Flat.destroy_all
+puts "Its empty at the moment"
+9.times do 
+puts "adding flats"
+  flat = Flat.create(
+  name: Faker::Address.city,
+  address: Faker::Address.street_address,
+  description: Faker::TvShows::Simpsons.quote,
+  price_per_night: rand(100.1000),
+  number_of_guests: rand(1..8)
+)
+end
+puts "it's done now"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
